95451a416eb9909bfc81aaaaf2b91bf0b059b2b7
Rudi Haryanto <rudiharyantovrt99@gmail.com>
2022-12-14 22:17:52 +0700

    class penerbangan

---
+++ ClassPenerbangan/model/ClassPenerbangan.java
@@ -0,0 +1,15 @@
+package ClassPenerbangan.model ;
+
+public class ClassPenerbangan {
+    private String nama;
+
+    public void setNama(String nama)
+    {
+        this.nama = nama;
+    }
+  
+    public String getNama()
+    {
+      return nama;
+    }
+}
