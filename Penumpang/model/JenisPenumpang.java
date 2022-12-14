14bb19c788b092afd45234e05d3f92e6f02f6d1b
daffaizno <naufaldaffa@gmail.com>
2022-12-14 20:13:44 +0700

    Menambahkan Model dan View "JenisPenumpang"

---
+++ Penumpang/model/JenisPenumpang.java
@@ -0,0 +1,23 @@
+package Penumpang.model;
+
+public class JenisPenumpang {
+    // ini masih belom tau apa aja isinya..
+    private String tiketDewasa;
+    private String tiketAnak;
+
+    public String getTiketDewasa() {
+        return tiketDewasa;
+    }
+
+    public void setTiketDewasa(String tiketDewasa) {
+        this.tiketDewasa = tiketDewasa;
+    }
+
+    public String getTiketAnak() {
+        return tiketAnak;
+    }
+
+    public void setTiketAnak(String tiketAnak) {
+        this.tiketAnak = tiketAnak;
+    }
+}
